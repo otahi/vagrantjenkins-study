ce3b0640f0642954b284e11a2c6d88980cc0757a
OTA Hiroshi <otahiros@gmail.com>
2013-06-02 13:06:01 +0900

    adds trema-default installed items

--- site-cookbooks/trema/recipes/default.rb
+++ site-cookbooks/trema/recipes/default.rb
@@ -2,7 +2,13 @@
 # Cookbook Name:: trema
 # Recipe:: default
 #
-# Copyright 2013, YOUR_COMPANY_NAME
-#
-# All rights reserved - Do Not Redistribute
-#
+
+%w{gcc make rubygems ruby-devel irb libpcap-devel sqlite-devel}.each do |pkg| package pkg do
+    action :install
+  end
+end
+
+%w{trema rspec}.each do |gem| gem_package gem do
+    action :install
+  end
+end
